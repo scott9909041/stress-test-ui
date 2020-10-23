aca9cee9bedf37f672fbf04608f3650cb6ee48fa
Scott Su <scott@osp.com.tw>
2020-10-23 10:37:59 +0800

    Aso api stress test model

---
+++ src/app/api.service.ts
@@ -0,0 +1,210 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject, timer } from 'rxjs';
+import { map, takeUntil, tap } from 'rxjs/operators';
+
+const DB_NAME = 'AsoStressTestDatabase';
+const TEST_OBJSTORE = 'tests';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  kill$: Subject<void> = new Subject();
+  complete$: Subject<void> = new Subject();
+
+  constructor(private http: HttpClient) {
+    this.setupDBConnection();
+    this.complete$.subscribe(() => this.storeMeta());
+  }
+
+  /**
+   * Test meta configuration
+   */
+  apiUrl: string;
+  interval: number;
+  apiNumber: number;
+  max: number;
+  startTime: Date;
+
+  /**
+   * Test result realted
+   */
+  counter: number = 0;
+  success: number = 0;
+  failure: number = 0;
+  meta: MetaData[] = [];
+  errors: any[] = [];
+  timerStopped: boolean;
+
+  /**
+   * IndexedDb
+   */
+  private db: IDBDatabase;
+
+  startTesting(apiUrl: string, interval: number, apiNumber: number, max: number): void {
+    // Reset
+    this.counter = 0;
+    this.success = 0;
+    this.failure = 0;
+    this.meta = [];
+    this.errors = [];
+    this.timerStopped = false;
+    // Configure
+    this.apiUrl = apiUrl;
+    this.interval = interval;
+    this.apiNumber = apiNumber;
+    this.max = max;
+    this.startTime = new Date();
+    // Start testing
+    timer(0, interval).pipe(
+      takeUntil(this.kill$),
+      tap(() => {
+        for (let count = 0; count < apiNumber; count++) {
+          if (this.counter === max) {
+            this.kill$.next();
+          } else {
+            this.get(apiUrl).subscribe(resp => this.handleResult(resp), error => this.handleError(error));
+            this.counter++;
+          }
+        }
+      })
+    ).subscribe({
+      complete: () => {
+        this.timerStopped = true;
+        this.checkComplete();
+      }
+    });
+  }
+
+  get(url: string): Observable<any> {
+    const startTime = new Date();
+    const headers: any = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: this.getToken()
+    };
+    return this.http.get(url, { headers }).pipe(
+      map(() => {
+        const metaData: MetaData = {
+          timecost: (new Date().getTime() - startTime.getTime()) / 1000
+        }
+        return metaData;
+      })
+    );
+  }
+
+  getToken(): string {
+    return 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2NzJDQUYyNy1GNTM2LTNEQzgtMzNCRC1BNTA5OTc3NUU4OTEiLCJpc3MiOiJBOERERTUzRC04QjBFLTI3OTgtMTNGMi0zMUU5OTZCODUxMTAiLCJzdWIiOiJPdHRvIEdyb3VwIiwiYXVkIjoiQVNPU1AiLCJpYXQiOjE1Nzc4MDgwMDAsImV4cCI6MTYwOTQzMDM5OX0.aZJQlkKBKinjap20azNF54t8SHc7tkPt4_5BIFeFKMw';
+  }
+
+  handleResult(result: MetaData): void {
+    this.success++;
+    this.meta.push(result);
+    this.checkComplete();
+  }
+
+  handleError(error: any): void {
+    this.failure++;
+    this.errors.push(error);
+    this.checkComplete();
+  }
+
+  checkComplete(): void {
+    if (this.timerStopped && this.counter === (this.success + this.failure)) {
+      this.complete$.next();
+    }
+  }
+
+  setupDBConnection(): void {
+    const request = indexedDB.open(DB_NAME);
+    request.onsuccess = event => {
+      console.log('createDB onsuccess');
+      this.db = event.target['result'];
+      try {
+        this.getHistory();
+      } catch (error) {
+        this.db.close();
+        this.updateDB(this.db.version + 1);
+      }
+    }
+  }
+
+  updateDB(version: number): void {
+    const request = indexedDB.open(DB_NAME, version);
+    request.onupgradeneeded = event => {
+      console.log('updateDB onupgradeneeded');
+      this.db = event.target['result'];
+      this.createObjects();
+      const transaction  = event.target['transaction'];
+      transaction.oncomplete = event => {
+        console.log('transaction oncomplete');
+      }
+    };
+  }
+
+  createObjects(): void {
+    // Create objects and indexes.
+    const testStore = this.db.createObjectStore(TEST_OBJSTORE, { autoIncrement : true });
+    testStore.createIndex('apiUrl', 'apiUrl', { unique: false });
+  }
+
+  getHistory(): Promise<TestMeta[]> {
+    return new Promise((resolve, reject) => {
+      const tests: TestMeta[] = [];
+      this.db.transaction([TEST_OBJSTORE]).objectStore(TEST_OBJSTORE).openCursor().onsuccess = (event) => {
+        const cursor = event.target['result'];
+        if (cursor) {
+          const test = cursor.value as TestMeta;
+          test.key = cursor.key;
+          tests.push(test);
+          cursor.continue();
+        }
+      };
+      resolve(tests);
+    });
+  }
+
+  storeMeta(): void {
+    const testStore = this.db.transaction([TEST_OBJSTORE], 'readwrite').objectStore(TEST_OBJSTORE);
+    const addRequest = testStore.add(this.getTestMeta(new Date()));
+    addRequest.onsuccess = event => {
+      //
+    }
+  }
+
+  getTestMeta(endTime: Date): TestMeta {
+    const meta: TestMeta = {
+      apiUrl: this.apiUrl,
+      apiNumber: this.apiNumber,
+      interval: this.interval,
+      totalNumber: this.max,
+      startTime: this.startTime,
+      endTime: endTime,
+      success: this.success,
+      failure: this.failure,
+      details: this.meta,
+      errors: this.errors
+    };
+    return meta;
+  }
+}
+
+export interface MetaData {
+  timecost: number;
+}
+
+export interface TestMeta {
+  apiUrl: string;
+  apiNumber: number;
+  interval: number;
+  totalNumber: number;
+  startTime: Date;
+  endTime: Date;
+  success: number;
+  failure: number;
+  details: MetaData[];
+  errors: any[];
+  key?: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stress-test';
+  
+  constructor(public apiService: ApiService) {}
+
+  isTetsing: boolean;
+
+  url: string = 'https://pos2dev.aso.com.tw/api/v2/shops';
+  duration: number = 1000;
+  number: string = '3';
+  max: string = '10';
+
+  apiList: string [] = [
+    'https://pos2dev.aso.com.tw/api/v2/shops',
+    'https://pos2dev.aso.com.tw/api/v2/members?size=10&page=1&mobile=0900000000',
+    'https://pos2dev.aso.com.tw/api/v2/members/CU170200001746',
+    'https://pos2dev.aso.com.tw/api/v2/products?size=10&page=1&simple=14291'
+  ]
+
+  durationOption = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000];
+
+  /**
+   * 
+   * Chart related setting
+   */
+  averageTime: any[];
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Time ranges';
+  showYAxisLabel = true;
+  yAxisLabel = 'API Count';
+
+  colorScheme = {
+    domain: ['#66bb6a', '#26c6da', '#42a5f5', '#ffca28', '#ef5350', '#ab47bc']
+  };
+
+  @HostListener('window:beforeunload', ['$event']) beforeUnload(event) {
+    // this.apiService.backup();
+    event.returnValue = '';
+  }
+
+  subscribtion: Subscription;
+
+  start(): void {
+    if (this.url && this.duration && this.number && this.max) {
+      if (this.subscribtion) {
+        this.subscribtion.unsubscribe();
+      }
+      this.apiService.startTesting(this.url, this.duration, +this.number, +this.max);
+      this.subscribtion = this.apiService.complete$.subscribe(() => {
+        this.isTetsing = false;
+        this.loadChart();
+      });
+      this.averageTime = null;
+      this.isTetsing = true;
+    }
+  }
+  
+  stop(): void {
+    this.apiService.kill$.next();
+    this.isTetsing = false;
+  }
+
+  loadChart(): void {
+    this.updateTimeCostChart();
+  }
+
+  updateTimeCostChart(): void {
+    const meta = this.apiService.meta;
+    const chart = [
+      {
+        name: '< 1s',
+        value: meta.filter(meta => meta.timecost < 1).length
+      },
+      {
+        name: '1s ~ 2s',
+        value: meta.filter(meta => 1 <= meta.timecost && meta.timecost < 2).length
+      },
+      {
+        name: '2s ~ 3s',
+        value: meta.filter(meta => 2 <= meta.timecost && meta.timecost < 3).length
+      },
+      {
+        name: '3s ~ 5s',
+        value: meta.filter(meta => 3 <= meta.timecost && meta.timecost < 5).length
+      },
+      {
+        name: '5s ~ 10s',
+        value: meta.filter(meta => 5 <= meta.timecost && meta.timecost < 10).length
+      },
+      {
+        name: '> 10s',
+        value: meta.filter(meta => meta.timecost >= 10).length
+      }
+    ];
+    this.averageTime = chart;
+  }
 }
